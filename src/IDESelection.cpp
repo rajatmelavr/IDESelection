f9306a444fb59b2a77008080b59277d63b8adcdd
Raj Prajapati <rajatmelavr@gmail.com>
2019-06-08 21:10:49 +0530

    ADDED: Main file to handle user Input
    Handling user input

---
+++ src/IDESelection.cpp
@@ -0,0 +1,18 @@
+/*
+ * IDESelection.cpp
+ *
+ *  Created on: 08-Jun-2019
+ *      Author: Raj Prajapati
+ *
+ *  Description: Main File of the IDE
+ */
+
+#include <iostream>
+
+int main (void)
+{
+	std::cout << "Welcome to IDE Selection " << std::endl;
+	return 0;
+}
+
+
